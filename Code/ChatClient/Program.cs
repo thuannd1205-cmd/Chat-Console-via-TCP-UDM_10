f8740aff27a65f11c8d441b10e5c02f8caaae1bc
anhvnv8076-sys <anhvnv8076@ut.edu.vn>
2026-04-03 18:26:41 +0700

    Implement basic chat client functionality

--- Code/ChatClient/Program.cs
+++ Code/ChatClient/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main()
+    {
+        Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        client.Connect(IPAddress.Parse("127.0.0.1"), 5000);
+
+        Console.Write("Nhập username: ");
+        string username = Console.ReadLine();
+
+        ClientLogic logic = new ClientLogic(client, username);
+
+        // Gửi login
+        logic.Send($"LOGIN|{username}||");
+
+        // Bắt đầu luồng nhận
+        logic.Start();
+
+        // Luồng gửi (main thread)
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input.StartsWith("@"))
+            {
+                string[] parts = input.Split(' ');
+                string receiver = parts[0].Substring(1);
+                string message = input.Substring(parts[0].Length + 1);
+
+                logic.Send($"MSG|{username}|{receiver}|{message}");
+            }
+            else
+            {
+                logic.Send($"ALL|{username}||{input}");
+            }
+        }
+    }
+}
