27669bc93028f194a36ba3debea67a1e6b335934
Thuan <thuan21026@gmail.com>
2026-04-16 17:26:30 +0700

    Xoa ChatCommon, Cap nhat code

--- Code/ChatCommon/Constants.cs
+++
@@ -1,9 +0,0 @@
-namespace ChatCommon
-{
-    public static class Constants
-    {
-        public const string ServerIP = "127.0.0.1";
-        public const int ServerPort = 8888;
-        public const int BufferSize = 4096;
-    }
-}
